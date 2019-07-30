50f42b58d5f0a9ce627dafee698fc739bb65f55f
Eric Xu <50253028+ericx20@users.noreply.github.com>
2019-07-30 19:57:09 -0400

    Update The Mirror.py

--- DMOJ/The Mirror.py
+++ DMOJ/The Mirror.py
@@ -1,20 +1,17 @@
 import math
 def checkPrime(n):
-    #print("Checking", n)
-    if n in [0, 1]:
-        #print("0 or 1 is not prime")
+    if n == 0 or n == 1:
         return False
     elif n == 2:
         return True
     else:
         isPrime = True
-        for divisor in range(2, math.ceil(math.sqrt(n))+1):
-            if n%divisor == 0:
-                #print("Factor found!", divisor)
+        for divisor in range(2, math.ceil(math.sqrt(n)) + 1):
+            if n % divisor == 0:
                 isPrime = False
                 break
-        #print("Is", n, "prime?", isPrime)
         return isPrime
+
 numOfCases = int(input())
 answerList = []
 for caseNum in range(numOfCases):
